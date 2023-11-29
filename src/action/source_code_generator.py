b6c77cbe110a1568e3a0628c937cc3eeb2b84d49
miyashita64 <javajavawookjavawook@gmail.com>
2023-11-29 09:40:48 +0900

    環境がバグったため念のためバックアップ

--- src/action/source_code_generator.py
+++ src/action/source_code_generator.py
@@ -1,5 +1,6 @@
 class SourceCodeGenerator:
     def generate(self, test_result):
+        # テスト結果をテスト対象毎にグルーピング
         test_targets = {}
         for testcase_result in test_result.testcase_results:
             if not testcase_result.is_passed:
@@ -7,6 +8,8 @@ class SourceCodeGenerator:
                 if test_target not in test_targets:
                     test_targets[test_target] = []
                 test_targets[test_target] += [testcase_result]
+        # テスト対象毎にソースコード生成
+        source_codes = []
         for test_target in test_targets:
             testcase_results = test_targets[test_target]
             failed_testcase_prompt = [testcase_result.code + "\n" + testcase_result.stdout for testcase_result in testcase_results]
