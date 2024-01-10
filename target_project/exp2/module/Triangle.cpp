436502ec426ad56301569103a3fca7982ffb7ec5
miyashita64 <javajavawookjavawook@gmail.com>
2024-01-10 20:38:53 +0900

    exp2-2

--- target_project/exp2/module/Triangle.cpp
+++ target_project/exp2/module/Triangle.cpp
@@ -6,4 +6,8 @@ std::string Triangle::determine_type(int side1, int side2, int side3)
     {
         return "正三角形";
     }
+    else if (side1 == side2 || side2 == side3 || side1 == side3)
+    {
+        return "二等辺三角形";
+    }
 }
--- target_project/exp2/test/TriangleTest.cpp
+++ target_project/exp2/test/TriangleTest.cpp
@@ -8,4 +8,9 @@ namespace tdd_sample_test
         EXPECT_STREQ(Triangle::determine_type(2, 2, 2).c_str(), "正三角形");
     }
 
+    // 二等辺三角形であることを判別できるか
+    TEST(TriangleTest, determineTypeScaleneTest){
+        EXPECT_STREQ(Triangle::determine_type(3, 2, 2).c_str(), "二等辺三角形");
+    }
+
 } // namespace tdd_sample_test
