69e176d1b165cc4242495ca50a91b9bf63ac0a0b
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-01-11 13:42:21 +0000

    exp2-3

--- target_project/exp2/module/Triangle.cpp
+++ target_project/exp2/module/Triangle.cpp
@@ -1,6 +1,7 @@
 #include "Triangle.h"
 #include <string>
 #include <iostream>
+#include <math.h>
 
 std::string Triangle::determine_type(int a, int b, int c){
     if (a == b && b == c){
@@ -9,5 +10,9 @@ std::string Triangle::determine_type(int a, int b, int c){
 
     if (a == c || a == b || b == c){
         return "二等辺三角形";
+    }
+
+    if (sqrt(a*a + b*b) == c || sqrt(b*b + c*c) == a || sqrt(a*a + c*c) == b){
+        return "直角三角形";
     }
 }
