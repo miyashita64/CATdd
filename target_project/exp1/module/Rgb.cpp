4e6138a9904b2a6e8099b2b1340524739c11f23e
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-01-11 11:36:13 +0000

    exp1-1

---
+++ target_project/exp1/module/Rgb.cpp
@@ -0,0 +1,3 @@
+#include "Rgb.h"
+
+Rgb::Rgb(int red, int green, int blue) : r(red), g(green), b(blue) {}
---
+++ target_project/exp1/module/Rgb.h
@@ -0,0 +1,11 @@
+#ifndef RGB_H
+#define RGB_H
+
+class Rgb {
+public:
+    int r, g, b;
+
+    Rgb(int red, int green, int blue);
+};
+
+#endif // RGB_H
