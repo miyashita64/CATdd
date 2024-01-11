718ac2a6ed35573c8e0bcbf8ad49ebe5a2df6e10
miyashita64 <javajavawookjavawook@gmail.com>
2024-01-11 16:39:24 +0900

    exp2-3

--- target_project/exp2/module/Triangle.cpp
+++ target_project/exp2/module/Triangle.cpp
@@ -8,5 +8,8 @@ std::string Triangle::determine_type(int a, int b, int c) {
     else if (a == b || b == c || a == c) {
         return "二等辺三角形";
     }
+    else if (a * a + b * b == c * c || b * b + c * c == a * a || a * a + c * c == b * b) {
+        return "直角三角形";
+    }
     return "不明";
 }
