9418f79e2487cd89eb05a01d569b9fdfe9657545
miyashita64 <javajavawookjavawook@gmail.com>
2024-01-11 16:36:41 +0900

    exp2-2

--- target_project/exp2/module/Triangle.cpp
+++ target_project/exp2/module/Triangle.cpp
@@ -5,5 +5,8 @@ std::string Triangle::determine_type(int a, int b, int c) {
     if (a == b && b == c) {
         return "正三角形";
     }
+    else if (a == b || b == c || a == c) {
+        return "二等辺三角形";
+    }
     return "不明";
 }
