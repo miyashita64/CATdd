0856b57457550a183edb05f50739652dd7d4994c
OnagaHaruki <haloupgarou@gmail.com>
2024-01-11 11:14:18 +0000

    “exp1-3”

--- target_project/exp1/module/Rgb.cpp
+++ target_project/exp1/module/Rgb.cpp
@@ -2,14 +2,21 @@
 #include "Rgb.h"
 
 Rgb::Rgb(int r, int g, int b) : r(r), g(g), b(b) {
-    // コンストラクタの実装
     if (this->r > 255) {
         this->r = 255;
+    } else if (this->r < 0) {
+        this->r = 0;
     }
+
     if (this->g > 255) {
         this->g = 255;
+    } else if (this->g < 0) {
+        this->g = 0;
     }
+
     if (this->b > 255) {
         this->b = 255;
+    } else if (this->b < 0) {
+        this->b = 0;
     }
 }
