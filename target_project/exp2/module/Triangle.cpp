5841272133eb4e4b4601b91ee8135159a98bcdf2
OnagaHaruki <haloupgarou@gmail.com>
2024-01-11 11:37:22 +0000

    “exp2-4”

--- target_project/exp2/module/Triangle.cpp
+++ target_project/exp2/module/Triangle.cpp
@@ -7,7 +7,9 @@ std::string Triangle::determine_type(int a, int b, int c) {
     } else if (a == b || b == c || a == c) {
         return "二等辺三角形";
     } else if (a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b*b) {
-        // ピタゴラスの定理を使用して直角三角形を判別{
+        // ピタゴラスの定理を使用して直角三角形を判別
         return "直角三角形";
+    } else {
+        return "三角形";
     }
 }
