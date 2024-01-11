642b1758fa46479ee3429b8b7450ded4775a963d
root <you@example.com>
2024-01-11 05:58:59 +0000

    exp1-2

--- target_project/exp1/module/Rgb.cpp
+++ target_project/exp1/module/Rgb.cpp
@@ -2,5 +2,8 @@
 #include "Rgb.h"
 
 Rgb::Rgb(int red, int green, int blue) : r(red), g(green), b(blue) {
+    // Adjust colors if they exceed the limit
+    if (r > 255) r = 255;
+    if (g > 255) g = 255;
+    if (b > 255) b = 255;
 }
-
