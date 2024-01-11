6fa10e96ba503601abc261062668d18be792e102
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-01-11 13:33:47 +0000

    exp2-2

--- target_project/exp2/module/Triangle.cpp
+++ target_project/exp2/module/Triangle.cpp
@@ -6,4 +6,8 @@ std::string Triangle::determine_type(int a, int b, int c){
     if (a == b && b == c){
         return "正三角形";
     }
+
+    if (a == c || a == b || b == c){
+        return "二等辺三角形";
+    }
 }
