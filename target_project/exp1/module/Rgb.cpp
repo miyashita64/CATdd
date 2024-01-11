12880a31c79f5e0f0c626709e714518034442aa7
OnagaHaruki <haloupgarou@gmail.com>
2024-01-11 11:06:24 +0000

    “exp1-1”

---
+++ target_project/exp1/module/Rgb.cpp
@@ -0,0 +1,6 @@
+
+#include "Rgb.h"
+
+Rgb::Rgb(int r, int g, int b) : r(r), g(g), b(b) {
+    // コンストラクタの実装
+}
---
+++ target_project/exp1/module/Rgb.h
@@ -0,0 +1,13 @@
+
+#ifndef RGB_H
+#define RGB_H
+
+class Rgb {
+public:
+    Rgb(int r, int g, int b);
+    int r;
+    int g;
+    int b;
+};
+
+#endif //RGB_H
