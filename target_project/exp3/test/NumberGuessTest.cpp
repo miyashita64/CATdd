385dbaed0fed3536e515e48218cb8303c2533d3d
miyashita64 <javajavawookjavawook@gmail.com>
2024-01-12 15:17:45 +0900

    exp3-2

--- target_project/exp3/test/NumberGuessTest.cpp
+++ target_project/exp3/test/NumberGuessTest.cpp
@@ -24,5 +24,24 @@ namespace tdd_sample_test
         // リダイレクトを元に戻す
         std::cin.rdbuf(originalCin);
     }
+    // 入力した値が一致しないことを判定できるか
+    TEST(NumberGuessTest, checkGuessFalseTest)
+    {
+        NumberGuess game;
+        // 正解の数を9に設定
+        int answer = 9;
+        game.setAnswer(answer);
+
+        // ユーザーが入力する数を0に設定
+        std::istringstream input("0");
+        std::streambuf* originalCin = std::cin.rdbuf();
+        std::cin.rdbuf(input.rdbuf());
+
+        // ユーザーの予想が正しいかどうかを確認
+        EXPECT_FALSE(game.checkGuess());
+
+        // リダイレクトを元に戻す
+        std::cin.rdbuf(originalCin);
+    }
 
 } // namespace tdd_sample_test
