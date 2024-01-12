9bdb63d4a6fd3fc43c6568c9db8ed2c34dc4d820
miyashita64 <javajavawookjavawook@gmail.com>
2024-01-12 14:13:18 +0900

    exp1-1

---
+++ target_project/exp1/module/Rgb.cpp
@@ -0,0 +1,8 @@
+// SampleClass.cpp
+#include "Rgb.h"
+
+Rgb::Rgb(int _r, int _g, int _b):r(_r), g(_g), b(_b){}
+
+// int Rgb::rgb(int r, int g, int b){
+// 	return ;
+// }
---
+++ target_project/exp1/module/Rgb.h
@@ -0,0 +1,16 @@
+// SampleClass.h
+#ifndef SAMPLE_CLASS_H
+#define SAMPLE_CLASS_H
+
+class Rgb{
+private:
+
+public:
+	Rgb(int _r, int _g, int _b);
+	int r;
+	int g;
+	int b;
+	// int rgb(int r, int g, int b);
+};
+
+#endif
